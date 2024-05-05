6912a5951e46891b803b5c7ce4aa4099320894c3
Nathan Craveiro <nathan.cga@outlook.com>
2024-05-05 18:22:49 -0300

    Adicionando javascript nas paginas quiz e questions

---
+++ src/javascript/questions.js
@@ -0,0 +1,93 @@
+export default [
+  {
+    question: "Qual é o inventor da bicicleta?",
+    answers: [
+      { option: "Karl Drais", correct: true },
+      { option: "Leonardo da Vinci", correct: false },
+      { option: "Thomas Edison", correct: false },
+      { option: "John Boyd Dunlop", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o componente da bicicleta responsável por mudar as marchas?",
+    answers: [
+      { option: "Câmbio", correct: true },
+      { option: "Freio", correct: false },
+      { option: "Pedal", correct: false },
+      { option: "Banco", correct: false },
+    ],
+  },
+  {
+    question: "O que é necessário para fazer um 'bunny hop'?",
+    answers: [
+      { option: "Levantar a roda dianteira da bicicleta", correct: true },
+      { option: "Fazer um giro de 360 graus", correct: false },
+      { option: "Andar em uma estrada de terra", correct: false },
+      { option: "Pulo de uma rocha", correct: false },
+    ],
+  },
+  {
+    question: "Qual é a distância percorrida em uma volta completa no Tour de France?",
+    answers: [
+      { option: "Aproximadamente 500 km", correct: false },
+      { option: "Aproximadamente 3.500 km", correct: true },
+      { option: "Aproximadamente 1.000 km", correct: false },
+      { option: "Aproximadamente 2.000 km", correct: false },
+
+    ],
+  },
+  {
+    question: "O que é uma bicicleta de estrada (road bike)?",
+    answers: [
+      { option: "Uma bicicleta projetada para estradas asfaltadas", correct: true },
+      { option: "Uma bicicleta de montanha", correct: false },
+      { option: "Uma bicicleta elétrica", correct: false },
+      { option: "Uma bicicleta que anda sobre a água", correct: false },
+    ],
+  },
+  {
+    question: "Qual destes não é um tipo de bicicleta?",
+    answers: [
+      { option: "Mountain bike", correct: false },
+      { option: "Roadster", correct: true },
+      { option: "Cruiser", correct: false },
+      { option: "V8", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o componente da bicicleta que converte a força das pernas em movimento?",
+    answers: [
+      { option: "Pedal", correct: true },
+      { option: "Corrente", correct: false },
+      { option: "Roda", correct: false }, 
+      { option: "Câmbio", correct: false },
+    ],
+  },
+  {
+    question: "O que é o Tour de France?",
+    answers: [
+      { option: "Uma corrida de motocicletas", correct: false },
+      { option: "Um evento de patinação no gelo", correct: false },
+      { option: "A maior competição de ciclismo do mundo", correct: true },
+      { option: "Uma competição entra motocicletas e corredores", correct: false },
+    ],
+  },
+  {
+    question: "Qual é o benefício do ciclismo para a saúde?",
+    answers: [
+      { option: "Melhora na saúde cardiovascular, redução do estresse, fortalecimento muscular, etc.", correct: true },
+      { option: "Aumento do risco de lesões", correct: false },
+      { option: "Diminuição da flexibilidade", correct: false },
+      { option: "Aumento da velocidade", correct: false },
+    ],
+  },
+  {
+    question: "O que é um pelotão?",
+    answers: [
+      { option: "Um tipo de bicicleta de corrida", correct: false },
+      { option: "Um acessório para bicicletas", correct: false },
+      { option: "Um grupo de ciclistas competindo juntos", correct: true },
+      { option: "Um competição", correct: false },
+    ],
+  },
+];
